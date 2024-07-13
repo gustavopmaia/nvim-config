d25446b2a380b58488ed6226a1c9c9b0c1cd7c3d
Gustavo Maia <gustavopmaia@hotmail.com>
2024-07-13 12:00:37 -0300

    more plugins

--- lua/gustavo/lazy.lua
+++ lua/gustavo/lazy.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("gustavo.plugins")
+require("lazy").setup({ { import = "gustavo.plugins" }, { import = "gustavo.plugins.lsp" } }, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false
+  }
+})
